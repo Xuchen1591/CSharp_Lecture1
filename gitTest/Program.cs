99583d31bc29d0490f5f9c982147135a107fe1df
magina.1591@outlook.com <magina.1591@outlook.com>
2015-03-18 20:47:28 +0800

    gittest

---
+++ gitTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gitTest
+{
+    //enum AllList
+    public enum TimeOfDay
+    {
+        morning = 0, 
+        noon = 1,
+        evening = 2
+    }
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1,2,3,4 };
+            foreach (int i in a)
+                //usage of 'foreach'
+                Console.WriteLine(i);
+
+            greeting(TimeOfDay.noon);
+            Console.WriteLine("{0}", TimeOfDay.morning);
+        }
+
+        static void greeting(TimeOfDay timeOfDay)
+        {
+            switch (timeOfDay)
+            {
+                case 0:
+                    Console.WriteLine("good morning");
+                    break;
+                case TimeOfDay.noon:
+                    Console.WriteLine("GOOd noon");
+                    break;
+            }
+        }
+    }
+}
